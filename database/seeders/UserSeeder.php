2ad0b12293cece989368ebf30eca013a9145efd3
Abdullah Al Shahed <abdullahalshahedkhan@gmail.com>
2025-05-15 19:40:29 +0600

    added user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'email' => 'admin@test.com',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+}
